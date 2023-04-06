95b02fbfa2780b4331ec02816d90eac159dbb8e0
Jeremy Gagnon <jeremyzgagnon@gmail.com>
2023-04-06 14:14:58 -0300

    requis terminer

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   resources :order_statuses
   resources :products
   resources :customers
-  resources :restaurants
+  resources :restaurants  
   resources :employees
   resources :addresses
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
