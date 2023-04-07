8bdf3091433384181fd926264d29b821e2cf4e05
Jeremy Gagnon <jeremyzgagnon@gmail.com>
2023-04-07 12:41:46 -0300

    tweak user athentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-    # before_action :authenticate_user!
-end
+    before_action :authenticate_user!
+  
+    private
+  
+    def authenticate_user!
+      unless user_signed_in? || request.path == root_path || request.path == new_user_session_path
+        redirect_to new_user_session_path, alert: "You must be logged in to access this page."
+      end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,11 @@ Rails.application.routes.draw do
   resources :restaurants  
   resources :employees
   resources :addresses
-  devise_for :users
-  devise_scope :user do  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+    }
+      devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
- end
-   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  end
   root to: "home#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
