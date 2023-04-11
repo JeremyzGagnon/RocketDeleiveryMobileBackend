5e5c7aed785f947ad6967f765ef3178c8f833553
Jeremy Gagnon <jeremyzgagnon@gmail.com>
2023-04-11 12:59:02 -0300

    seeding succeded

--- db/migrate/20230408141203_create_courier_statuses.rb
+++ db/migrate/20230408141203_create_courier_statuses.rb
@@ -7,10 +7,10 @@ class CreateCourierStatuses < ActiveRecord::Migration[7.0]
     end
       add_index :courier_statuses, :name, unique: true
 
-      CourierStatus.create(name: 'free')
-      CourierStatus.create(name: 'busy')
-      CourierStatus.create(name: 'full')
-      CourierStatus.create(name: 'offline')
+      # CourierStatus.create(name: 'free')
+      # CourierStatus.create(name: 'busy')
+      # CourierStatus.create(name: 'full')
+      # CourierStatus.create(name: 'offline')
     
   end
 end
