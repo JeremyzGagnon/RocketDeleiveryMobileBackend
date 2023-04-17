97e948f5e5d287ee756ac51611c93634db572835
Jeremy Gagnon <jeremyzgagnon@gmail.com>
2023-04-17 11:59:20 -0300

    Login path response changed

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -6,9 +6,14 @@ module Api
             email = data["email"]
             password = data["password"]
             user = User.find_by(email: email)
-            # puts user
+            user_id = user.id
+            customer = Customer.find_by(user_id: user_id)
+            customer_id = customer.id
+            courier = Courier.find_by(user_id: user_id)
+            courier_id = courier.id
+
             if user && user.valid_password?(password)
-                render json: { success: true}
+                render json: { success: true, user_id: user_id, customer_id: customer_id, courier_id: courier_id}
             else
                 render json: { success: false }, status: :unauthorized
             end
