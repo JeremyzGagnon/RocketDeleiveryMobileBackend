d13ddb6b0c3897c54f68956f601c52813bc9e667
Jeremy Gagnon <jeremyzgagnon@gmail.com>
2023-04-29 18:14:50 -0300

    fixed orders index again

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -10,16 +10,28 @@ module Api
             user = User.find_by(email: email)
             user_id = user.id
             customer = Customer.find_by(user_id: user_id)
-            customer_id = customer.id
             courier = Courier.find_by(user_id: user_id)
-            courier_id = courier.id
+
+            # customer_id = customer.id
+            # courier_id = courier.id
 
             if user && user.valid_password?(password)
-                render json: { success: true, user_id: user_id, customer_id: customer_id, courier_id: courier_id}
-            else
+                if customer && !courier
+                  customer_id = customer.id
+                  render json: { success: true, user_id: user_id, customer_id: customer_id }
+                elsif courier && !customer
+                  courier_id = courier.id
+                  render json: { success: true, user_id: user_id, courier_id: courier_id }
+                elsif customer && courier
+                  customer_id = customer.id
+                  courier_id = courier.id
+                  render json: { success: true, user_id: user_id, customer_id: customer_id, courier_id: courier_id }
+                end
+              else
                 render json: { success: false }, status: :unauthorized
+              end        
             end
-        end
+        
         
     end
     
